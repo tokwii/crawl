9ceef6f66fc2f3faefb6eea5630e2f4b57e094b1
Timothy Okwii <tokwii@cisco.com>
2017-12-15 16:32:28 +0000

    Removed Comments

--- crawl.go
+++ crawl.go
@@ -13,11 +13,6 @@ import (
 const CONFIG_FILE  = "config/settings.toml"
 
 func main()  {
-	// If the Buffer is Smaller than the links found. It will be blocked ... To try to read from the Buffer
-	// Faster than we write... The Read on Buffer blocks when it is full
-	// Recieve on the buffer is is blocked when it is emtpy
-	// We need some sort of Pool of fetchers to read continoulys for the worker Q until empty
-	// Crazy !! Create a go routine for each taskQ
 
 	err := config.Conf.LoadConfig(CONFIG_FILE)
 
